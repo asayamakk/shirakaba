4a56299f6321830c5e3cbb6e4860d44e53334905
浅山広大 <kodai419419@gmail.com>
2016-05-19 02:25:59 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,145 @@
+const electron = require('electron');
+// Module to control application life.
+const {app} = electron;
+// Module to create native browser window.
+const {BrowserWindow} = electron;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function() {
+  createWindow();
+  installMenu();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+function installMenu() {
+  var Menu = require('menu');
+  if(process.platform == 'darwin') {
+    menu = Menu.buildFromTemplate([
+      {
+        label: 'shirakaba',
+        submenu: [
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click: function() { app.quit(); }
+          },
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Reload',
+            accelerator: 'Command+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Toggle Full Screen',
+            accelerator: 'Ctrl+Command+F',
+            click: function() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+          },
+        ]
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            role: 'undo'
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            role: 'redo'
+          },
+          {
+            type: 'separator'
+          },
+          {
+            label: 'Cut',
+            accelerator: 'CmdOrCtrl+X',
+            role: 'cut'
+          },
+          {
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy'
+          },
+          {
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste'
+          },
+          {
+            label: 'Select All',
+            accelerator: 'CmdOrCtrl+A',
+            role: 'selectall'
+          },
+        ]
+      }
+    ]);
+    Menu.setApplicationMenu(menu);
+  } else {
+    menu = Menu.buildFromTemplate([
+      {
+        label: '&View',
+        submenu: [
+          {
+            label: '&Reload',
+            accelerator: 'Ctrl+R',
+            click: function() { mainWindow.restart(); }
+          },
+          {
+            label: 'Toggle &Full Screen',
+            accelerator: 'F11',
+            click: function() { mainWindow.setFullScreen(!mainWindow.isFullScreen()); }
+          },
+        ]
+      }
+    ]);
+    mainWindow.setMenu(menu);
+  }
+
+}
---
+++ tokenizer.js
@@ -0,0 +1,108 @@
+var DIC_URL = "bower_components/kuromoji/dist/dict/";
+
+var tokenizer = null;
+
+
+var vm = new Vue({
+    el: "#demo",
+    data: {
+        inputText: "",
+        tokens: [],
+        isLoading: true,
+        message: "Loading dictionaries ...",
+        svgStyle: "hidden"
+    },
+    methods: {
+        tokenize: function () {
+            if (vm.inputText == "" || tokenizer == null) {
+                vm.tokens = [];
+                // lattice = null;
+                return;
+            }
+            try {
+                // lattice = tokenizer.getLattice(vm.inputText);
+                vm.tokens = tokenizer.tokenize(vm.inputText);
+            } catch (e) {
+                console.log(e);
+                // lattice = null;
+                vm.tokens = [];
+            }
+        }
+    }
+});
+
+
+// フォームの内容が変化したらtokenizeする
+vm.$watch("inputText", function (value) {
+    vm.svgStyle = "hidden";
+    vm.tokenize();
+});
+
+
+// Load and prepare tokenizer
+kuromoji.builder({ dicPath: DIC_URL }).build(function (error, _tokenizer) {
+    if (error != null) {
+        console.log(error);
+    }
+    tokenizer = _tokenizer;
+
+    vm.message = "Ready";
+
+    vm.inputText = "すもももももももものうち";
+    vm.isLoading = false;
+    document.getElementById('export_csv').className = "btn btn-default"
+});
+
+var button = document.getElementById('export_csv');
+button.onclick = function() {
+  var content = build_csv();
+  var blob = new Blob([ content ], { type: "text/csv;charset=utf-16;" });
+  this.href = window.URL.createObjectURL(blob);
+  this.download = vm.tokens[0].surface_form + "_tokens.csv";
+}
+
+
+function build_csv() {
+  var csv_string = "";
+
+  var header = ['表層形','品詞','品詞細分類1','品詞細分類2','品詞細分類3','活用型','活用形','基本形','読み','発音'].join("\t")
+
+  csv_string += header;
+  csv_string += '\r\n';
+
+  for(var i = 0; i < vm.tokens.length; i++) {
+    var token = vm.tokens[i];
+    csv_string += ([token.surface_form, token.pos, token.pos_detail_1, token.pos_detail_2, token.pos_detail_3, token.conjugated_type, token.conjugated_form, token.basic_form, token.reading, token.pronunciation]).join("\t");
+    csv_string += '\r\n';
+  }
+
+  // BOM追加
+  csv_string = "\ufffe" + csv_string;
+
+  // if (isLittleEndian()) {
+
+  //   //実行環境のエンディアンがLEならTypedArrayを利用
+  //   var array = [];
+  //   for (var i=0; i<csv_string.length; i++){
+  //       array.push(csv_string.charCodeAt(i));
+  //   }
+  //   var csv_contents = new Uint16Array(array);
+
+  // } else {
+
+  //   //LEでない場合はDataViewでUTF-16LEのArrayBufferを作成
+  //   var array_buffer = new ArrayBuffer(csv_string.length * 2);
+  //   var data_view = new DataView(array_buffer);
+  //   for (var i=0,j=0; i<csv_string.length; i++,j=i*2) {
+  //       data_view.setUint16( j, csv_string.charCodeAt(i), true ); //第3引数にtrueを渡すとLEになる
+  //   }
+  //   var csv_contents = array_buffer
+  // }
+
+  return csv_string;
+}
+
+function isLittleEndian(){
+    if ((new Uint8Array((new Uint16Array([0x00ff])).buffer))[0]) return true;
+    return false;
+}
